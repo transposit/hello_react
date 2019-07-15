6b76caf33bf5bc10788c97414284ec715858106e
tina <tina@transposit.com>
2019-07-15 19:18:33 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -2,6 +2,7 @@
   const parsed_body = http_event.parsed_body;
   const workspaceId = parsed_body.team_id;
   const userId = parsed_body.user_id;
+  const response_url = parsed_body.response_url;
 
   setImmediate(() => {
     let user = api.user({type: "slack", workspaceId, userId});
