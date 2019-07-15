9d1c6ed8fd9d16c5f224c5d2fdd241fe96e1fab6
tina <tina@transposit.com>
2019-07-15 03:24:33 +0000

    Initial service boilerplate

---
+++ src/hello_world.js
@@ -0,0 +1,17 @@
+(params) => {
+  return [
+    {
+      language: "english",
+      message: "Hello, world"
+    },
+    {
+      language: "spanish",
+      message: "Hola, mundo"
+    }
+  ];
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
