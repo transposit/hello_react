254e9240a78e37959d3472c0913b7905a12b38b4
daisie_local <daisieh@gmail.com>
2019-10-25 14:47:27 -0700

    update webhook to use new version

--- src/webhook.js
+++ src/webhook.js
@@ -9,22 +9,17 @@
   const parsed_body = http_event.parsed_body;
   const workspaceId = parsed_body.team_id;
   const userId = parsed_body.user_id;
-  const response_url = parsed_body.response_url;
 
   setImmediate(() => {
     let user = api.user({type: "slack", workspaceId, userId});
     if (user) {
       let message = api.run('this.get_slack_message', {}, {asUser: user.id})[0];
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: message
-      });      
+      api.run("slack_webhook.respond_to_slash_command", { http_event, message });
     } else {
-      api.run("slack_webhook.post_to_response_url", {
-        response_url: response_url,
-        post_body: {text: 'Please configure your user at ' + env.getBuiltin().appUrl}
-      });      
+      let text = `Configure user settings at ${env.getBuiltin().appUrl}`;
+      api.run("slack_webhook.respond_to_slash_command", { http_event, text });
     }
   });
-  return { status_code: 200 };
+  return api.run("slack_webhook.acknowledge_slash_command");
 }
+
